ff275a1b9e5822e751749fdce5fba2d430724a19
Alexandre <102592431+Foxenz@users.noreply.github.com>
2023-06-06 20:28:44 +0200

    Initialisation du projet

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Pistache</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
